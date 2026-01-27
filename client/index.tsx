482c7bf3d9d6622bf361c41eba21e6d84f80e8a9
Sofia Tarn <doseoftheplagues@gmail.com>
2026-01-27 14:09:00 +1300

    update auth config

--- client/index.tsx
+++ client/index.tsx
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
     <Auth0Provider
       domain="https://sofia-tarn.au.auth0.com"
       clientId="mqCCZ4X7323TwP6uuAnaDLluqG6mQNSG"
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
       authorizationParams={{
         redirect_uri: window.location.origin,
         audience: 'https://test/api',
