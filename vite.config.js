fd7ea53211f65c1c4edb42f5da4b01165efa556a
Sofia Tarn <doseoftheplagues@gmail.com>
2026-01-27 16:33:28 +1300

    fix viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Ensure base path is set correctly
+  base: '/',
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '/symbols': path.resolve(__dirname, './public/symbols')
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
     allowedHosts: ['naida-unbestowed-annice.ngrok-free.dev'],
   },
-  publicDir: 'public', // Explicitly define public directory
+  publicDir: 'public',
 })
